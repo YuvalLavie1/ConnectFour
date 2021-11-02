b7eabc039a7ec753383670f28b2af7a41c53595e
unknown <yuvallavie1@gmail.com>
2021-11-02 10:57:21 +0200

    Documentation and rearrange functions' location

--- connectFour.pl
+++ connectFour.pl
@@ -1,26 +1,108 @@
-del([X|Xs], X,Xs):-!.
-del([Y|Ys], X,[Y|Zs]):-!,
-	del(Ys,X,Zs).
-del(_,[],[]).
+% Const
+size(8).
+
+startGame(Difficulty):-
+	size(S), createBoard(S, Board), mainLoop(Board, Difficulty).
 
 
-% createBoard(8, B), alphabeta(max, B, -1000, 1000, 6, GoodPos, Val).
+% Program always starts, and plays as the max player.
+% After choosing the next move, it prints the new board and then
+% checks if won. If so, stops the game. Else, gets user's input
+% and prints the board. Checks again if user won. If not, does it
+% all over again until there's a wining or a draw.
+mainLoop(Board, Difficulty):-
+	alphabeta(max, Board, -1000, 1000, Difficulty, NewBoard, _), 
+	printBoard(NewBoard), 
+	(printMessageIfWin(NewBoard, o),!
+	;
+	createBoardFromUser(NewBoard, NewBoard1),
+	printBoard(NewBoard1), (printMessageIfWin(NewBoard1, x),!
+	;mainLoop(NewBoard1, Difficulty))).
+
+
+%%%%%%%%%%%%%%% Create empty board and print board helpers %%%%%%%%%%%%%%%
+createBoard(Size, Board):-
+	createBoard(Size, Size, Board).
+
+% Stop condition
+createBoard(_, 0, []):-!.
+
+% Creates one line at a time
+createBoard(Size, LineNumber, Lines):-
+	createLine(LineNumber, Size, Line),
+	LineNumber1 is LineNumber-1,
+	createBoard(Size, LineNumber1, Rest),
+    append(Line, Rest, Lines).
+
+% Stop condition
+createLine(_, 0, []):-!.
+% Creates one node at a time
+createLine(LineNumber, AmountOfNodes, [Node|Nodes]):-
+	%Column-Row-Type
+	Node = AmountOfNodes-LineNumber-empty,
+	AmountOfNodes1 is AmountOfNodes-1,
+	createLine(LineNumber, AmountOfNodes1, Nodes).
+
+printBoard(Board):-
+	size(Size),
+	printLineNumbers(Size), nl,
+	printBoard(Board, Size).
+
+% Stop condition
+printBoard(_, 0):-!.
+% Prints one line at a time
+printBoard(Board, LineNumber):-
+	size(Size),
+	printLine(LineNumber, Size, Board), nl,
+	LineNumber1 is LineNumber-1,
+	printBoard(Board, LineNumber1).
+
+% Stop condition
+printLine(_, 0, _):-!.
+% Prints one node at a time
+printLine(LineNumber, NodeNumber, Board):-
+	member(NodeNumber-LineNumber-Object, Board),
+	% For empty nodes it prints a blank
+    ((Object==empty, write(" "),!);
+	write(Object)), write("|"),
+	NodeNumber1 is NodeNumber-1,
+	printLine(LineNumber, NodeNumber1, Board).
+
+% Stop condition
+printLineNumbers(0):-!.
+% Prints columns' numbers
+printLineNumbers(LineNumber):-
+	write(LineNumber), write(" "),
+	LineNumber1 is LineNumber-1,
+	printLineNumbers(LineNumber1).
+
+%%%%%%%%%%%%%%% Alphabeta algorithm %%%%%%%%%%%%%%%
 
 nextTurn(max, min).
 nextTurn(min, max).
 
 
+/* 
+ * alphabeta(Turn, Pos, Alpha, Beta, Depth, GoodPos, Val)
+ * Like the algorithm mentioned in the book, but with a few changes:
+ * a. greather(less)-equal instead of just greater(less) then helps 
+ *    improve the performance by not checking the latter functions.
+ * b. Depth support was added.
+ * c. The way of detection min/max turn.
+*/
+
+% If it's a wining/losing position, there's no need to check the
+% moves after, and then return a value (rank).
 alphabeta(_, Pos, _, _, _, _, XRank):-
 	isWin(Pos, Player), !,
 	((Player==o, XRank=1000) ; 
 	(Player==x, XRank= -1000)).
 
-		
+% For 0 depth, we'll get the value from our heuristic function
 alphabeta(_, Pos, _, _, 0, _, Val) :-
-	!, heuristic(Pos, Val).%, write(Pos), nl, write(Val).
+	!, heuristic(Pos, Val).
 
 alphabeta(Turn, Pos, Alpha, Beta, Depth, GoodPos, Val) :-
-	%write(Depth),nl,
 	D1 is Depth -1, 
 	moves(Pos, Turn, PosList),
     nextTurn(Turn, NTurn),
@@ -56,101 +138,72 @@ betterof(Turn, Pos, Val, _, Val1, Pos, Val):-
 
 betterof(_, _, _, Pos1, Val1, Pos1, Val1).
 	
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-size(8).
-
+% Gets all the possible boards after one move
+% and returns it in the Moves list
 moves(Board, Turn, Moves):-
 	((Turn==min, Turn1=x,!) ; Turn1=o) ,
 	setof(M, X^move(Board, Turn1, M, X), Moves).
 
+% Get one possible boarrd if X is var, and checks if it's 
+% a legal move if no vars were given.
 move(Board, Turn, NewBoard, X):-
 	size(S),   
 	member(X-Y-empty, Board), Y=<S, 
-	(Y==1 ;
-	% There's something beneath Y '%
+	(Y==1 ; % It's the first row
+	% There's something beneath Y
 	(Y1 is Y-1, member(X-Y1-Taken, Board), Taken\=empty)),
-    del(Board, X-Y-empty, NewBoard1), append(NewBoard1, [X-Y-Turn], NewBoard).
+    delete(Board, X-Y-empty, NewBoard1), append(NewBoard1, [X-Y-Turn], NewBoard).
+
+%%%%%%%%%%%%%%%%%%%%%%%%% heuristic helpers %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% The max player's sign is o, so the value is calculated by
+% substruction of x player's rank from o's one.
 heuristic(Board, Value):-
-	getRank1(Board, o, ORank), 
-	getRank1(Board, x, XRank),
+	getRank(Board, o, ORank), 
+	getRank(Board, x, XRank),
 	Value is ORank-XRank.
-/*
+
+% Rank of a player is the sum of his rank in horizontal quartet, 
+% vertical one, and each direction of the diagonal quartet.
+% For each quartet type it sums up it's value using aggregate_all method.
 getRank(Board, Player, Rank):-
-	aggregate_all(count, getHorizontal3AndEmpty(Board, Player), HR),
-	aggregate_all(count, getVertical3AndEmpty(Board, Player), VR),
-	aggregate_all(count, getRightDiagonal3AndEmpty(Board, Player), RDR),
-	aggregate_all(count, getLeftDiagonal3AndEmpty(Board, Player), LDR),
-	Rank is HR+VR+LDR+RDR.
-
-getHorizontal3AndEmpty(Board, Player):-
-	member(X1-Y-Player, Board), X2 is X1+1,
-	member(X2-Y-Player, Board), X3 is X2+1, 
-	member(X3-Y-Player, Board), (   X4 is X3+1 ; X4 is X1-1), 
-	member(X4-Y-empty, Board).
-
-getVertical3AndEmpty(Board, Player):-
-	member(X-Y1-Player, Board), Y2 is Y1+1,
-	member(X-Y2-Player, Board), Y3 is Y2+1, 
-	member(X-Y3-Player, Board), Y4 is Y3+1, 
-	member(X-Y4-empty, Board).
-
-getRightDiagonal3AndEmpty(Board, Player):-
-	member(X1-Y1-Player, Board), Y2 is Y1+1, X2 is X1+1,
-	member(X2-Y2-Player, Board), Y3 is Y2+1, X3 is X2+1,
-	member(X3-Y3-Player, Board), 
-	((Y4 is Y3+1, X4 is X3+1) ;  (Y4 is Y1-1, X4 is X1-1)),
-	member(X4-Y4-empty, Board).
-
-getLeftDiagonal3AndEmpty(Board, Player):-
-	member(X1-Y1-Player, Board), Y2 is Y1+1, X2 is X1-1,
-	member(X2-Y2-Player, Board), Y3 is Y2+1, X3 is X2-1,
-	member(X3-Y3-Player, Board), 
-	((Y4 is Y3+1, X4 is X3-1) ;  (Y4 is Y1-1, X4 is X1+1)),
-	member(X4-Y4-empty, Board).
-*/
-
-getRank1(Board, Player, Rank):-
 	aggregate_all(sum(Rank1), isMatch(Board, getQuartetRightDiagonal, Player, Rank1), HR),
 	aggregate_all(sum(Rank2), isMatch(Board, getQuartetLeftDiagonal, Player, Rank2), VR),
 	aggregate_all(sum(Rank3), isMatch(Board, getQuartetHorizontal, Player, Rank3), RDR),
 	aggregate_all(sum(Rank4), isMatch(Board, getQuartetVertical, Player, Rank4), LDR),
 	Rank is HR+VR+LDR+RDR.
 
-
-
+% Creates quartet by the type that was given (function), and then
+% return the appropiate rank by calling helpers functions.
 isMatch(Board, Function, Player, Rank):-
 	call(Function, Board, Q), countEmptyAndPlayer(Q, Player, EmptyCount, PlayerCount),
 	getQuartetRank(EmptyCount, PlayerCount, Rank).
 
+% Checks all of the quartet types if there's a win 
 isWin(Board, Player):-
 	getQuartetRightDiagonal(Board, Quartet),
-	sameFour(Quartet, Player),
-	Player \= empty,!.
+	sameFour(Quartet, Player),!.
 isWin(Board, Player):-
 	getQuartetLeftDiagonal(Board, Quartet),
-	sameFour(Quartet, Player),
-	Player \= empty,!.
+	sameFour(Quartet, Player),!.
 isWin(Board, Player):-
 	getQuartetHorizontal(Board, Quartet),
-	sameFour(Quartet, Player),
-	Player \= empty,!.
+	sameFour(Quartet, Player),!.
 isWin(Board, Player):-
 	getQuartetVertical(Board, Quartet),
-	sameFour(Quartet, Player),
-	Player \= empty,!.	
+	sameFour(Quartet, Player).
+
+sameFour([P, P, P, P], P):-
+	P \= empty.
 
 % One empty and three player earnes 2 points
 getQuartetRank(1, 3, 2):-!.
 % Two empty and two player earnes 1 points
 getQuartetRank(2, 2, 1):-!.
 
-sameFour([P, P, P, P], P).
-
-
+% Counts the amount of empty nodes and player's nodes.
+% If the opposite player's node is encountered, it fails.
 countEmptyAndPlayer([], _, 0, 0):-!.
 countEmptyAndPlayer([empty|Rest], Player, EmptyCount, PlayerCount):-
 	countEmptyAndPlayer(Rest, Player, EmptyCount1, PlayerCount),
@@ -160,6 +213,7 @@ countEmptyAndPlayer([Player|Rest], Player, EmptyCount, PlayerCount):-
 	countEmptyAndPlayer(Rest, Player, EmptyCount, PlayerCount1),
     PlayerCount is PlayerCount1+1,!.
 
+% Get all of the quartet types
 getQuartetRightDiagonal(Board, [Player1, Player2, Player3, Player4]):-
 	member(X1-Y1-Player1, Board), Y2 is Y1+1, X2 is X1+1,
 	member(X2-Y2-Player2, Board), Y3 is Y2+1, X3 is X2+1,
@@ -184,71 +238,18 @@ getQuartetVertical(Board, [Player1, Player2, Player3, Player4]):-
 	member(X-Y3-Player3, Board), Y4 is Y3+1, 
 	member(X-Y4-Player4, Board).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%% User interactive %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%%%%%%%%%%%%%%%%%%%%%%%%
-createBoard(Size, Board):-
-	createBoard(Size, Size, Board).
-createBoard(_, 0, []):-!.
-createBoard(Size, LineNumber, Lines):-
-	createLine(LineNumber, Size, Line),
-	LineNumber1 is LineNumber-1,
-	createBoard(Size, LineNumber1, Rest),
-    append(Line, Rest, Lines).
-
-createLine(_, 0, []):-!.
-createLine(LineNumber, AmountOfNodes, [Node|Nodes]):-
-	Node = AmountOfNodes-LineNumber-empty,
-	AmountOfNodes1 is AmountOfNodes-1,
-	createLine(LineNumber, AmountOfNodes1, Nodes).
-
-printBoard(Board):-
-	size(Size),
-	printLineNumbers(Size), nl,
-	printBoard(Board, Size).
-printBoard(_, 0):-!.
-printBoard(Board, LineNumber):-
-	size(Size),
-	printLine(LineNumber, Size, Board), nl,
-	LineNumber1 is LineNumber-1,
-	printBoard(Board, LineNumber1).
-
-printLine(_, 0, _):-!.
-printLine(LineNumber, NodeNumber, Board):-
-	member(NodeNumber-LineNumber-Object, Board),
-	((Object==empty, write(" "),!);
-	write(Object)), write("|"),
-	NodeNumber1 is NodeNumber-1,
-	printLine(LineNumber, NodeNumber1, Board).
-
-printLineNumbers(0):-!.
-printLineNumbers(LineNumber):-
-	write(LineNumber), write(" "),
-	LineNumber1 is LineNumber-1,
-	printLineNumbers(LineNumber1).
-
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
+% Prints appropriate message for a win or a lose 
 printMessageIfWin(Board, o):-
 	isWin(Board, o), write("Sorry, you lost"),!.
 printMessageIfWin(Board, x):-
 	isWin(Board, x), write("Great! You won!").
 
+% Gets requested column from user, validates it and 
+% when it's valid returns the new board         ' 
 createBoardFromUser(Board, NewBoard):-
 	repeat, (write("Choose column 1-8:"),nl,
-	read(X), move(Board, x, NewBoard, X),!
-	;write("Illeagal move :("), fail).
+	read(X), move(Board, x, NewBoard, X),! % Validation
+	;write("Illeagal move :("), nl, fail).
 
-startGame(Difficulty):-
-	size(S), createBoard(S, Board), mainLoop(Board, Difficulty).
-
-
-mainLoop(Board, Difficulty):-
-	alphabeta(max, Board, -1000, 1000, Difficulty, NewBoard, _), 
-	printBoard(NewBoard), 
-	(printMessageIfWin(NewBoard, o),!
-	;
-	createBoardFromUser(NewBoard, NewBoard1),
-	printBoard(NewBoard1), (printMessageIfWin(NewBoard1, x),!
-	;mainLoop(NewBoard1, Difficulty))).
